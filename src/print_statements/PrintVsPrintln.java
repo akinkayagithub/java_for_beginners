3781a205d53abae567e0b76a014b206a18abd1ac
akinkayagithub <akinkaya.b@gmail.com>
2022-09-08 15:41:26 -0500

    added print statements

---
+++ src/print_statements/PrintVsPrintln.java
@@ -0,0 +1,18 @@
+package print_statements;
+
+public class PrintVsPrintln {
+    public static void main(String[] args) {
+
+        //println
+        System.out.println("J");
+        System.out.println("A");
+        System.out.println("V");
+        System.out.println("A");
+
+        //print
+        System.out.print("J");
+        System.out.print("A");
+        System.out.print("V");
+        System.out.print("A");
+    }
+}
