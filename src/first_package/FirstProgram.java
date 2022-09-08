67e27f6ae2b6e9a2f54396d82a84bcf5a04ba6c4
akinkayagithub <akinkaya.b@gmail.com>
2022-09-08 15:40:31 -0500

    first program

---
+++ src/first_package/FirstProgram.java
@@ -0,0 +1,8 @@
+package first_package;
+
+public class FirstProgram {
+    //main method is the start point for the execution
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
