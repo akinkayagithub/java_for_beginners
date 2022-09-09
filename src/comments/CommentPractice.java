12fa37bddb5486459eb3da636bef4d1a6d26bed7
akinkayagithub <akinkaya.b@gmail.com>
2022-09-09 12:14:19 -0500

    added comments

---
+++ src/comments/CommentPractice.java
@@ -0,0 +1,20 @@
+package comments;
+
+public class CommentPractice {
+    public static void main(String[] args) {
+
+        //System's println() method is used to print some text or variables, and it moves to next line
+        System.out.println("Hello World"); // Hello World
+        System.out.println("I like Java"); // This line should print I like Java
+
+        /*
+        All your comments here
+        Another comment
+         */
+
+        /**
+         * Documentation comments
+         * Another comments
+         */
+    }
+}
