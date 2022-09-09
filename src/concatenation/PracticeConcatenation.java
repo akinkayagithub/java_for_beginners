ca572cc90db32f23467c987f303960e2a628c018
akinkayagithub <akinkaya.b@gmail.com>
2022-09-09 13:39:19 -0500

    added concatenation

---
+++ src/concatenation/PracticeConcatenation.java
@@ -0,0 +1,14 @@
+package concatenation;
+
+public class PracticeConcatenation {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World"); // Hello World
+        System.out.println("Hello " + "World"); // Hello World
+        System.out.println("Hello" + " World"); // Hello World
+        System.out.println("Hello" + " " + "World"); // Hello World
+
+        System.out.println("I practice Java");
+        System.out.println("I" + " " + "practice" + " " + "Java");
+    }
+}
