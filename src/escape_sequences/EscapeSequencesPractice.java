5bc00a3a281abba6967e2fe82f831de759e002bd
akinkayagithub <akinkaya.b@gmail.com>
2022-09-09 13:20:32 -0500

    added escape sequences

---
+++ src/escape_sequences/EscapeSequencesPractice.java
@@ -0,0 +1,22 @@
+package escape_sequences;
+
+public class EscapeSequencesPractice {
+    public static void main(String[] args) {
+        /*
+            Java is a high-level, class-based, object-oriented programming
+        language that is designed to have as few implementation dependencies as possible.
+
+            It is a general\purpose programming language intended to let
+        programmers write once, run anywhere "WORA", meaning that compiled Java
+        code can run on all platforms that support Java without the need to
+        recompile.
+         */
+
+        System.out.println("\tJava is a high-level, class-based, object-oriented programming\n" +
+                "language that is designed to have as few implementation dependencies as possible.\n\n" +
+                "\tIt is a general\\purpose programming language intended to let\n" +
+                "programmers write once, run anywhere \"WORA\", meaning that compiled Java\n" +
+                "code can run on all platforms that support Java without the need to\n" +
+                "recompile.");
+    }
+}
